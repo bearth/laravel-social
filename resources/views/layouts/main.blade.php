2f8d8e90360be6e92a0363c2d33ee81a054fa21e
Aggression <bordin.hongprapat@gmail.com>
2016-11-20 11:01:15 +0700

    try using Vue in Laravel project

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,12 +8,13 @@
 </head>
 <body>
     @include('includes.header')
-    <div class="container">
+    <div id="app" class="container">
         @yield('content')
     </div>
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
